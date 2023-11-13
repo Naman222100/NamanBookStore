386daac70eb2a84eb8cfb4323c04b600a00f6259
W0822254 <W0822254@ADL-A1201G-06>
2023-11-13 17:06:37 -0500

    Added migration files

--- NamanBooks.DataAccess/Data/ApplicationDbContext.cs
+++ NamanBooks.DataAccess/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using NamanBooks.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,8 @@ namespace NamanBookStore.DataAccess.Data
             : base(options)
         {
         }
+
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
---
+++ NamanBooks.Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace NamanBooks.Models
+{
+    public class Category
+    {
+            [Key]
+            public int ID { get; set; }
+
+            [Display(Name = "Category Name")]
+            [Required]
+            [MaxLength(50)
+        }
+}
